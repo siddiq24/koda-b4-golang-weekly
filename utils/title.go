50c118eaf6c903954cce9d9936e7acfcaada8923
siddiq24 <siddiqrm24@gmail.com>
2025-10-23 05:20:51 +0700

    feat: add reusable title style

---
+++ utils/title.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func Title(title string) {
+	lgt := len(title)
+	space := "   "
+	dashN := lgt + 6 + 10
+	dash := ""
+	for range dashN{
+		dash += "="
+	}
+
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+	fmt.Println(dash)
+	fmt.Printf("=====%s%s%s=====\n", space, title, space)
+	fmt.Printf("%s\n\n",dash)
+}
