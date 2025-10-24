c6d2754ab7225febb87c8ebf86c3a311b7fb3f72
siddiq24 <siddiqrm24@gmail.com>
2025-10-25 06:18:01 +0700

    feat: add util to display struk implement go routine with 1. waitGroup for waiting all go routine to fitish running. 2. channel, to enter data sequentially so that it produces sequential data too

---
+++ utils/showBill.go
@@ -0,0 +1,102 @@
+package utils
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/siddiq24/golang-weekly/models"
+)
+
+func ShowStruk(bills []models.Bill, id int) {
+	var wg sync.WaitGroup
+	c := ""
+	d := ""
+	s := "     "
+	w := 40
+	space := ""
+	dash := ""
+	totI := 0
+	totP := uint(0)
+	stot := ""
+	spay := ""
+	scha := ""
+	for i := range w {
+		if i < 28-len(strconv.Itoa(int(bills[id].Total))) {
+			stot += " "
+		}
+		if i < 23-len(strconv.Itoa(int(bills[id].Bayar))) {
+			spay += " "
+		}
+		if i < 27-len(strconv.Itoa(int(bills[id].Change))) {
+			scha += " "
+		}
+		if i < w/2-2 {
+			space += " "
+		}
+		if i < w {
+			dash += "-"
+		}
+		c += string(rune(30))
+		d += string(rune(31))
+	}
+	Print(fmt.Sprintf("\n\n%s %s", s, c))
+	// ============================================================
+	Print(fmt.Sprintf("%s|%sBILL%s|", s, space, space))
+	Print(fmt.Sprintf("%s|%s|", s, dash))
+	Print(fmt.Sprintf("%s| Order Number: %s           |", s, bills[id].Invoice))
+	Print(fmt.Sprintf("%s| %s                        |", s, bills[id].Date))
+	Print(fmt.Sprintf("%s| %s                           |", s, bills[id].Time))
+	Print(fmt.Sprintf("%s|%s|", s, dash))
+
+	iniChan := make(chan string, len(bills[id].Carts)*3)
+
+	for _, cart := range bills[id].Carts {
+		wg.Add(1)
+		go withGor(cart, &totI, &totP, w, s, space, &wg, iniChan)
+	}
+
+	go func() {
+		wg.Wait()
+		close(iniChan)
+	}()
+
+	for ch := range iniChan {
+		Print(ch)
+	}
+
+	Print(fmt.Sprintf("%s|%s%s    |", s, space, space))
+	Print(fmt.Sprintf("%s|%s|", s, dash))
+	Print(fmt.Sprintf("%s|  Total Item %d                          |", s, totI))
+	Print(fmt.Sprintf("%s|%s|", s, dash))
+	Print(fmt.Sprintf("%s|  Total%sRp.%d  |", s, stot, totP))
+	Print(fmt.Sprintf("%s|  Payment:                              |", s))
+	Print(fmt.Sprintf("%s|     %s%sRp.%d  |", s, bills[id].Payment, spay, bills[id].Bayar))
+	Print(fmt.Sprintf("%s|  Change%sRp.%d  |", s, scha, bills[id].Change))
+	Print(fmt.Sprintf("%s|%s%s    |", s, space, space))
+	Print(fmt.Sprintf("%s|%s%s    |", s, space, space))
+	// ============================================================
+	Print(fmt.Sprintf("%s %s\n", s, d))
+}
+
+func Print(s string) {
+	fmt.Println(s)
+	time.Sleep(100 * time.Millisecond)
+}
+
+func withGor(cart models.Cart, totI *int, totP *uint, w int, s, space string, wg *sync.WaitGroup, ch chan string) {
+	defer wg.Done()
+	total := uint(cart.Qty) * cart.Price
+	*totI += cart.Qty
+	*totP += total
+	sn := w - 2 - len(cart.Name)
+	spaceName := ""
+	for range sn {
+		spaceName += " "
+	}
+
+	ch <- fmt.Sprintf("%s|%s%s    |", s, space, space)
+	ch <- fmt.Sprintf("%s|  %s%s|", s, cart.Name, spaceName)
+	ch <- fmt.Sprintf("%s|     %d X %d                Rp.%d  |", s, cart.Qty, cart.Price, total)
+}
