dca03233b28a21feb5d4786cb4713e1505ea99c7
siddiq24 <siddiqrm24@gmail.com>
2025-10-25 06:25:53 +0700

    feat: implementing the same method name for different structs

--- models/model.go
+++ models/model.go
@@ -1,10 +1,12 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Product struct {
 	Name  string
-	Price int
+	Price uint
 }
 
 var Products []Product = []Product{
@@ -33,10 +35,51 @@ var Products []Product = []Product{
 
 func (p Product) PrintProduct(i int) {
 	spaceI := "   "
-	if i>9{spaceI = "  "}
+	if i > 9 {
+		spaceI = "  "
+	}
 	spaceN := ""
-	for range 33-len(p.Name){
+	for range 33 - len(p.Name) {
 		spaceN += " "
 	}
-	fmt.Printf("%d%s| %s%s|  Rp.%d\n", i,spaceI,p.Name,spaceN,p.Price)
+	fmt.Printf("%d%s| %s%s|  Rp.%d\n", i, spaceI, p.Name, spaceN, p.Price)
+}
+
+func (c Cart) PrintProduct(i int) {
+	spaceI := "   "
+	if c.Id > 9 {
+		spaceI = "  "
+	}
+	spaceN := ""
+	for range 33 - len(c.Name) {
+		spaceN += " "
+	}
+	spaceQ := " "
+	if c.Qty > 9 {
+		spaceQ = ""
+	}
+	if i < 0 {
+		fmt.Printf("    |                                  |       |\n")
+	} else {
+		fmt.Printf("%d%s| %s%s|   %d%s  |  Rp.%d\n", i, spaceI, c.Name, spaceN, c.Qty, spaceQ, c.Total)
+	}
+}
+
+type Cart struct {
+	Id    int
+	Name  string
+	Price uint
+	Qty   int
+	Total int
+}
+
+type Bill struct {
+	Carts   []Cart
+	Invoice string
+	Date    string
+	Time    string
+	Payment string
+	Total   uint
+	Bayar   uint
+	Change  uint
 }
