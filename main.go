a413128ba3a6492854ce34ba2dd963895c6453b6
siddiq24 <siddiqrm24@gmail.com>
2025-10-25 06:53:07 +0700

    fix: finxing recover so that when a panic occurs it returns to home

--- main.go
+++ main.go
@@ -8,32 +8,39 @@ import (
 	"github.com/siddiq24/golang-weekly/utils"
 )
 
+var data pages.Db
+
 func main() {
 	defer func() {
 		if r := recover(); r != nil {
-			msg := "\n" + fmt.Sprint(r) + "\nClose Program. . ."
+			msg := "\n" + fmt.Sprint(r) + "\n>> Back to HOME...."
 			utils.Alert(msg)
+			main()
 		}
 	}()
 
 	for {
 		utils.Title("Welcome to My System")
 		fmt.Println("   1. Menu\n   2. Cart\n   3. History")
-		fmt.Println("\n   0. Exit")
+		fmt.Println("\n   x. Exit")
 		fmt.Printf("\nChoose a menu: ")
 		var input string
 		fmt.Scanln(&input)
 
 		switch input {
-		case "0":
+		case "":
+			main()
+		case "x":
 			utils.Alert("Exitting...")
 			os.Exit(0)
 		case "1":
-			pages.MenuPage()
+			data.MenuPage()
 		case "2":
+			data.Cart()
 		case "3":
+			data.History()
 		default:
-			utils.Alert("\n>>> 404 Not Found. . . ")
+			continue
 		}
 	}
 }
